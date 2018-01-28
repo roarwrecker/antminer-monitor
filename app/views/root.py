00f3d98a4d61070789676db832ae1efda89b63a6
roarwrecker <helmut.rohregger@gmail.com>
2018-01-28 15:29:38 +0100

    pass unicode string.

--- app/views/root.py
+++ app/views/root.py
@@ -6,6 +6,6 @@ from app import app
 @app.route('/')
 def root():
     request_ip = request.remote_addr
-    if ipaddress.ip_address(request_ip).is_loopback:
+    if ipaddress.ip_address(unicode(request_ip)).is_loopback:
         return redirect(url_for('details'))
     return redirect(url_for('temperature'))
